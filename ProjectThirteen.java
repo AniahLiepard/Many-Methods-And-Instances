7ad03fd04273a4ba5c19eaca761172a9f9d3f03e
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 10:29:42 -0400

    Many Methods And Instances Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17981

---
+++ ProjectThirteen.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ProjectThirteen {
+	Scanner typeOut = new Scanner(System.in);
+	ProjectThirteenPart2 item = new ProjectThirteenPart2();
+	System.out.println("What kind of sweets do you want?");
+	String typeIn = input.nextLine();
+	item.getSweets(item);
+	item.text();
+}
---
+++ ProjectThirteenPart2.java
@@ -0,0 +1,14 @@
+public class ProjectThirteenPart2 {
+	private String sweets;
+	public void faveSweets(String treats[]){
+		sweets = treats;
+		
+		public String getSweets(){
+			return sweets;
+		}
+		
+		public void text(){
+			System.out.printf("I'll give you %s", text());
+		}
+	}
+}
